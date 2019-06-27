a9ae38761834ccd03126f8b253943760f82a81d5
guilindev <guilindev@gmail.com>
2019-06-26 22:40:37 -0400

    Spring Boot Web 1

---
+++ 2_SpringBoot_Web/src/main/java/com/guilin/web/web/WebController.java
@@ -0,0 +1,47 @@
+package com.guilin.web.web;
+
+import com.guilin.web.model.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class WebController {
+    @RequestMapping(name="/getUser", method= RequestMethod.POST)
+    public User getUser() {
+        User user = new User();
+        user.setName("小明");
+        user.setAge(12);
+        user.setPass("123456");
+        return user;
+    }
+
+    @RequestMapping("/getUsers")
+    public List<User> getUsers() {
+
+        List<User> users = new ArrayList<>();
+        User user1 = new User();
+        user1.setName("neo");
+        user1.setAge(30);
+        user1.setPass("neo123");
+        users.add(user1);
+
+        User user2 = new User();
+        user2.setName("小明");
+        user2.setAge(12);
+        user2.setPass("123456");
+        users.add(user2);
+
+        return users;
+    }
+
+    // 使用url进行传参，通过url传递参数给get()方法的String name
+    @RequestMapping(value = "get/{name}", method = RequestMethod.GET)
+    public String get(@PathVariable String name) {
+        return name;
+    }
+}
