4d549dba661972e0a487d12cf6b9553c414857fe
guilindev <guilindev@gmail.com>
2019-07-02 22:25:28 -0400

    Add Unit tests for controllers integrated by swagger

--- 10_SpringBoot_swagger2/src/main/java/com/guilin/controller/MessageController.java
+++ 10_SpringBoot_swagger2/src/main/java/com/guilin/controller/MessageController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//Api 作用在 Controller 类上，做为 Swagger 文档资源，该注解将一个 Controller（Class）标注为一个 Swagger 资源（API）。
 @Api(value = "消息", description = "消息操作 API", position = 100, protocols = "http")
 @RestController
 @RequestMapping("/")
@@ -18,7 +19,7 @@ public class MessageController {
     @Autowired
     private  MessageRepository messageRepository;
 
-    @ApiOperation(
+    @ApiOperation( //ApiOperation 定义在方法上，描述方法名、方法解释、返回信息、标记等信息。
             value = "消息列表",
             notes = "完整的消息内容列表",
             produces="application/json, application/xml",
@@ -35,6 +36,8 @@ public class MessageController {
             notes = "根据参数创建消息"
     )
     @ApiImplicitParams({
+            // @ApiImplicitParams 用于描述方法的返回信息，和 @ApiImplicitParam 注解配合使用；
+            // @ApiImplicitParam 用来描述具体某一个参数的信息，包括参数的名称、类型、限制等信息
             @ApiImplicitParam(name = "id", value = "消息 ID", required = true, dataType = "Long", paramType = "query"),
             @ApiImplicitParam(name = "text", value = "正文", required = true, dataType = "String", paramType = "query"),
             @ApiImplicitParam(name = "summary", value = "摘要", required = false, dataType = "String", paramType = "query"),
@@ -52,6 +55,8 @@ public class MessageController {
     )
     @PutMapping(value = "message")
     @ApiResponses({
+            // @ApiResponses 主要封装方法的返回信息和 @ApiResponse 配置起来使用，
+            // @ApiResponse 定义返回的具体信息包括返回码、返回信息等。
             @ApiResponse(code = 100, message = "请求参数有误"),
             @ApiResponse(code = 101, message = "未授权"),
             @ApiResponse(code = 103, message = "禁止访问"),
@@ -79,8 +84,5 @@ public class MessageController {
     public void delete(@PathVariable("id") Long id) {
         this.messageRepository.deleteMessage(id);
     }
-
-
-
 }
 
