976e54a69ef10cb9361779bde902ba01a4a26258
guilindev <guilindev@gmail.com>
2019-07-04 15:20:42 -0400

    Spring Boot JDBC with multiple data sources

---
+++ 12_SpringBoot_jdbc/spring-boot-multi-jdbc/src/test/java/com/guilin/repository/UserRepositoryTests.java
@@ -0,0 +1,27 @@
+package com.guilin.repository;
+
+import com.guilin.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JdbcTemplate primaryJdbcTemplate;
+    @Autowired
+    private JdbcTemplate secondaryJdbcTemplate;
+
+    @Test
+    public void testSave() {
+        User user = new User("clark","admin_root",23);
+        userRepository.save(user, primaryJdbcTemplate);
+        userRepository.save(user, secondaryJdbcTemplate);
+    }
+}
