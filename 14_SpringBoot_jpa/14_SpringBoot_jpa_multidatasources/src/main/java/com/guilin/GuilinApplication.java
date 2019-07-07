0958cbf4cc9791da025b25ce9e01ae0ce6b2ce2e
guilindev <guilindev@gmail.com>
2019-07-06 20:50:56 -0400

    spring boot with jpa multi sources

---
+++ 14_SpringBoot_jpa/14_SpringBoot_jpa_multidatasources/src/main/java/com/guilin/GuilinApplication.java
@@ -0,0 +1,13 @@
+package com.guilin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuilinApplication.class, args);
+    }
+
+}
