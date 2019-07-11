994381c04021bfe99705ec0cd2194436331e510f
guilindev <guilindev@gmail.com>
2019-07-10 23:15:44 -0400

    Spring Boot integrated to Redis

---
+++ 18_SpringBoot_redis/spring-boot-redis-session-1/src/main/java/com/guilin/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.guilin.repository;
+
+import com.guilin.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserName(String userName);
+}
