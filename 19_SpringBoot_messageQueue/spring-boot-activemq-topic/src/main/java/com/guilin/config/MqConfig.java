270191ba289b21bf171cd931eb2f18f548c85014
guilindev <guilindev@gmail.com>
2019-07-11 21:54:14 -0400

    Spring Boot with ActiveMQ topic

--- 19_SpringBoot_messageQueue/spring-boot-activemq-topic/src/main/java/com/guilin/config/MqConfig.java
+++ 19_SpringBoot_messageQueue/spring-boot-activemq-topic/src/main/java/com/guilin/config/MqConfig.java
@@ -1,4 +1,18 @@
 package com.guilin.config;
 
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+
+@Configuration
 public class MqConfig {
+    @Bean
+    public Topic topic() {
+        return new ActiveMQTopic("guilin.topic");
+    }
 }
---
+++ 19_SpringBoot_messageQueue/spring-boot-activemq-topic/src/main/java/com/guilin/consumer/Consumer.java
@@ -0,0 +1,14 @@
+package com.guilin.consumer;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "guilin.topic")
+    public void receiveTopic(String text) {
+        System.out.println("Consumer topic msg : "+text);
+    }
+
+}
