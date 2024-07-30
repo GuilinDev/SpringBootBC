df5827ea21179df1fe6403ec4802a8419400a3e1
Guilin Zhang <GuilinDev@gmail.com>
2024-07-29 22:31:32 -0400

    Upgrade to java 21, boot 3.3.2, and dockerize

--- 1_SpringBoot_hello/src/test/java/com/neo/hello/web/HelloTest.java
+++ 1_SpringBoot_hello/src/test/java/com/neo/hello/web/HelloTest.java
@@ -1,9 +1,8 @@
 package com.neo.hello.web;
 
-import com.neo.hello.web.HelloController;
 import org.hamcrest.Matchers;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,7 +15,7 @@ public class HelloTest {
 
     private MockMvc mockMvc;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
     }
